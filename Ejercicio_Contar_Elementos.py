53937916fee4eacf67e4590980270a93a9896daf
DavidjmSanz <davidjmsanchez@gmail.com>
2025-05-21 00:02:44 -0500

    actualizacion ejercicios for

---
+++ Ejercicio_Contar_Elementos.py
@@ -0,0 +1,5 @@
+numeros=[3,7,2,9,4]
+contador=0
+for numero in numeros:
+    contador+=1
+print("la cantidad de numeros es: ", contador)
---
+++ Sumar_Elementos_Lista.py
@@ -0,0 +1,5 @@
+lista=[5,10,15,20,25]
+suma=0
+for numero in lista:
+    suma+=numero
+    print(suma)
